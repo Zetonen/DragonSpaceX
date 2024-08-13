4232a5bbfd1d164fbecd470fae000a6eda35778c
Seven <76436489+Zetonen@users.noreply.github.com>
2024-08-13 03:57:52 -0700

    vite, image, fonts, styles, npm modules

---
+++ src/components/GlobalStyles.jsx
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+import "modern-normalize";
+import "reset-css";
+import { colors } from "../constants/colors";
+
+export const GlobalStyles = createGlobalStyle`
+body {  
+  color: ${colors.WHITE};
+  font-family: "Rubik";
+  font-size: 14px;
+}
+ul,
+ol {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+img {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+button, a {
+font-family: inherit;
+font-size: inherit;
+color: inherit;
+}
+a {
+    text-decoration: none;
+}`;
