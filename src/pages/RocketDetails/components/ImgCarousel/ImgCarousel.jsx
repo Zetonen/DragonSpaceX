ed9f7f2ef46d4bb365fe2730fa78d346ca35bd12
Seven <76436489+Zetonen@users.noreply.github.com>
2024-08-15 08:57:38 -0700

    Update ImgCarousel.jsx

--- src/pages/RocketDetails/components/ImgCarousel/ImgCarousel.jsx
+++ src/pages/RocketDetails/components/ImgCarousel/ImgCarousel.jsx
@@ -1,9 +1,15 @@
 import { Img, SwiperStyled, ThumbImg } from "./ImgCarousel.styled";
 import { SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 
 export const ImgCarousel = ({ slides, name }) => {
   return (
-    <SwiperStyled spaceBetween={0} slidesPerView={0.98}>
+    <SwiperStyled
+      pagination={true}
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={0}
+      slidesPerView={1}
+    >
       {slides.map((slide, i) => (
         <SwiperSlide key={i}>
           <ThumbImg>
