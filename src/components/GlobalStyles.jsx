63c9926dd1b5001f5f2b23c5da94f745f97227c2
Seven <76436489+Zetonen@users.noreply.github.com>
2024-08-16 18:28:08 -0700

    Update GlobalStyles.jsx

--- src/components/GlobalStyles.jsx
+++ src/components/GlobalStyles.jsx
@@ -6,7 +6,7 @@ import { colors } from "../constants/colors";
 export const GlobalStyles = createGlobalStyle`
 body {  
   color: ${colors.WHITE};
-  font-family: "Rubik";
+  font-family: "Rubik", sans-serif;
   font-size: 14px;
   font-weight: 400;
   background: ${colors.BLACK};
